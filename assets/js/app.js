d78d067100bd4c3b4422a1cdba2ab499cc4c8516
AlvarezF7 <alvarez.vic.382@gmail.com>
2025-12-03 21:03:13 -0300

    corrige sintaxis

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,15 @@
 
 
- let diametro = Number(prompt("Ingrese un diámetro (cm): "));
- radio = diametro/2;
+let diametro = Number(prompt("Ingrese un diámetro (cm): "));
+let radio = diametro/2;
 
- const area = Math.PI*Math.pow(radio,2);
+let area = Math.PI*Math.pow(radio,2);
+ 
 //mensaje en consola
 console.log(area);
 
 window.alert(` Para un diametro de ${diametro}, el área es: ${area.toFixed(2)} cm² `);
-document.getElementById("result").textContent =
-    (`Para un diametro de ${diametro}, el área es : ${area.toFixed(2)} cm²`);
+
+const resultado = document.getElementById("result");
+resultado.innerHTML =`Para un diametro de ${diametro}, el área es : ${area.toFixed(2)} cm²`;
 
